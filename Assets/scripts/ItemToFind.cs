9f3e3855f6b996d0b80919d5206b6f93d34fd334
NekkoMaru <102283939+NekkoMaru@users.noreply.github.com>
2024-03-21 14:14:51 +0200

    Add teleport/ jump movement / parkour section

---
+++ Assets/scripts/ItemToFind.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform destination;
+    public GameObject playerGameObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            MazeGenerator.Instance.DisappearMaze();
+
+            // Only need for teleport
+            playerGameObject.SetActive(false);
+
+            // Teleport position
+            player.position = destination.position;
+
+            // Only need for teleport
+            playerGameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/scripts/key.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    // Статические переменные для ссылок на игрока, пункт назначения и игровой объект игрока
+    public static Transform player;
+    public static Transform destination;
+    public static GameObject playerGameObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Деактивация игрового объекта игрока
+            playerGameObject.SetActive(false);
+
+            // Телепортация игрока на указанную позицию
+            player.position = destination.position;
+
+            // Активация игрового объекта игрока
+            playerGameObject.SetActive(true);
+        }
+    }
+}
